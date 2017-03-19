40640dbd5d7ad3e323304ae8fcb4161a0cf24b71
Jaime Bronozo <bronozoj@gmail.com>
2017-03-19 12:13:27 +0800

    friday class materials up to date

--- FLMFUV/me03/me03.c
+++ FLMFUV/me03/me03.c
@@ -7,12 +7,30 @@
 
 #include <stdio.h>
 
+#define TERM_LIMIT 80
+
 int main(void)
 {
-	int period, loop;
+	int period, loop, a, b;
 
+	// prompt for user input
 	printf("Enter period: ");
 	scanf(" %d", &period);
 
-	
+	// initializes marker draw positions to top and goes to bottom
+	for(a = (period/2), b = a ; a >= 0; a--, b++){
+
+		// stops when character limit is reached
+		for(loop = 0; loop < TERM_LIMIT; loop++){
+
+			// a and b stores what part of the signal is to be drawn
+			if(loop % period  == a || loop % period == b)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n"); // to draw next line
+	}
+
+	return 0;
 }
---
+++ FLMFUV/me06/README.md
@@ -0,0 +1,13 @@
+# Machine Exercise 6
+
+## SMS Text
+
+Create a C Program that prints the SMS version of a string by removing each vowel in a word. If a vowel is the start of a word it is not removed.
+
+Bonus (2pts): Improve by removing all extra spaces.
+
+`The quick       brown fox jumps over the lazy dog`
+
+`Th qck brwn fx jmps ovr th lzy dg`
+
+[Solution](me06.c)
