bf8775982888e9a05a0cf2d540aab1f8a7f462b4
Jaime Bronozo <bronozoj@gmail.com>
2017-03-24 13:15:42 +0800

    me07 added

---
+++ FLMFUV/me07/me07.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char input[512];
+	int index;
+
+	// prompt for user input
+	printf("Input string: ");
+	fgets(input, sizeof(input), stdin);
+
+	for(index = 0; input[index] != '\0'; index++){
+		// pressed enter
+		if(input[index] == '\n')
+			break;
+
+		// opening tag detected
+		if(input[index] == '<'){
+			// skip until closing tag is detected
+			while(input[index] != '>' && input[index] != '\0')
+				index++;
+		}
+		// remove double space
+		else if(input[index] == ' ' && input[index+1] == '<')
+		{}
+		// print since valid
+		else
+			printf("%c", input[index]);
+	}
+	printf("\n");
+
+	return 0;
+}
